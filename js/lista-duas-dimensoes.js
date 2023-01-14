f62574dc9cb84ce9ba59499814c2ec96e0385654
Jethro <jethrotiago@hotmail.com>
2023-01-14 08:44:46 -0300

    Adicionando lista-duas-dimensoes

--- js/lista-duas-dimensoes.js
+++ js/lista-duas-dimensoes.js
@@ -1,5 +1,5 @@
-const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const alunos = ['João', 'Maria', 'Pedrinho', "Lucas"];
 const medias = [10, 8, 7.5, 9];
 
-const listaDeAlunosEMedias = [alunos, medias];
-console.log(listaDeAlunosEMedias);
+const listaAlunosEMedias = [alunos, medias];
+console.log(listaAlunosEMedias);

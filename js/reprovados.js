a52541c0c9d4a304efc7bf4d107100f5f3b4370e
Jethro <jethrotiago@hotmail.com>
2023-01-18 08:00:55 -0300

    Adicionado testes de filter

--- js/reprovados.js
+++ js/reprovados.js
@@ -1,4 +1,22 @@
 const alunos = ['Ana', 'Marcos', 'Maria', 'Mauro'];
-const notas = [7, 4.5, 8, 7.5];
+const medias = [7, 4.5, 8, 7.5];
 
-const reprovados = alunos.filter();
+const reprovados = alunos.filter((aluno, indice) => {
+    return true;
+});
+
+console.log(reprovados); //console: ['Ana', 'Marcos', 'Maria', 'Mauro']
+
+//Em caso de um return false
+//console.log(reprovados); irá exibir []
+
+//Exemplo 2:
+
+const alunos = ['Ana', 'Marcos', 'Maria', 'Mauro'];
+const medias = [7, 4.5, 8, 7.5];
+
+const reprovados = alunos.filter((_, indice) => {  //É preciso existir algo no primeiro parametro para poder acessar o segundo
+    return medias[indice] < 7;
+});
+
+console.log(reprovados);//['Marcos']

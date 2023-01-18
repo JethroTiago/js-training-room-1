deddd838379762a621c1d8ad9e3f2555063ddc4a
Jethro <jethrotiago@hotmail.com>
2023-01-18 08:18:28 -0300

    Reduzindo verbosidade em testes de reduce

--- js/media-geral.js
+++ js/media-geral.js
@@ -3,9 +3,7 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(notasDaSala) {
-    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => {
-        return acumulador + nota;
-    }, 0);
+    const somaDasNotas = notasDaSala.reduce((acc, nota) => acc + nota, 0);
 
     const media = somaDasNotas / notasDaSala.length;
 
@@ -15,3 +13,5 @@ function calculaMedia(notasDaSala) {
 console.log(`A média da sala de JavaScript foi de ${calculaMedia(salaJS)}`);
 console.log(`A média da sala de Java foi de ${calculaMedia(salaJava)}`);
 console.log(`A média da sala de Python foi de ${calculaMedia(salaPython)}`);
+
+//Acumulador foi renomeado para acc

7a1209ab6942ffded789f38bc462b4ea349c7855
Jethro <jethrotiago@hotmail.com>
2023-02-19 20:35:27 -0300

    Criado array com numeros de telefone dentro do objeto

--- js/objeto.js
+++ js/objeto.js
@@ -3,8 +3,9 @@
 const objeto = {
     nome: "Jethro",
     idade: 38,
-    cpf: 22214430539
+    cpf: "22214430539"
 }
 
 console.log(objeto.nome);
 console.log(`O nome da pessoa é ${objeto.nome} e ele tem ${objeto.idade} anos`);
+console.log(`Os três primeiros números do cpf são: ${objeto.cpf.substring(0, 3)}`);
---
+++ js/tipos.js
@@ -0,0 +1,6 @@
+const cliente = {
+    nome: "JOão",
+    idade: 25,
+    email: "joão@email.com",
+    telefone: ["11992969091", "11998746856"],
+}

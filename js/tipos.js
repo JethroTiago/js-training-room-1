882e8943a4ae6b95cdfa76d60b8c62a85d0cfb11
Jethro <jethrotiago@hotmail.com>
2023-02-19 20:51:30 -0300

    Inserindo umarray com um objeto dentro de outro array dentro do objeto

--- js/tipos.js
+++ js/tipos.js
@@ -5,10 +5,19 @@ const cliente = {
     telefone: ["11992969091", "11998746856"],
 }
 
-cliente.endereco = {
-    rua: "Rouxinol",
-    casa: true,
-    numero: 435,
-}
+cliente.enderecos = [
+    {
+        rua: "Rouxinol",
+        casa: true,
+        numero: 435,
+    }
+];
+
+
+cliente.enderecos.push({
+    rua: "Gralha Azul",
+    casa: false,
+    numero: 487,
+});
 
 console.log(cliente);

060592b6b9ec4a3fad6cbae70a7d1ad05919c41a
Jethro <jethrotiago@hotmail.com>
2023-02-19 21:10:57 -0300

    Adicionado funcao ao objeto

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,1 +1,17 @@
-const
+const cliente = {
+    nome: "João",
+    idade: 25,
+    email: "joão@email.com",
+    telefone: ["11992969091", "11998746856"],
+    saldo: 200,
+    efetuaPagamento: function(valor) {
+        if (valor > this.saldo) {
+            console.log("Saldo insuficiente")
+        } else {
+            this.saldo -=valor;
+            console.log(`Pagamento realizado. Novo saldo é igual a ${this.saldo}`)
+        }
+    },
+};
+
+cliente.efetuaPagamento(150);

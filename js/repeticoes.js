b087242155f25757b92772a91d9169175b592b7a
Jethro <jethrotiago@hotmail.com>
2023-02-21 10:27:21 -0300

    Criado repeticoes.js

---
+++ js/repeticoes.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "João",
+    idade: 25,
+    email: "joão@email.com",
+    telefone: ["11992969091", "11998746856"],
+}
+
+cliente.enderecos = [
+    {
+        rua: "Rouxinol",
+        casa: true,
+        numero: 435,
+    }
+];

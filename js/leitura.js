96164bc7dd5958dd5b1d363242911be03141c3ec
Jethro <jethrotiago@hotmail.com>
2023-02-22 11:17:30 -0300

    Utilizando JSON.stringfy para transformar o objeto em string

--- js/leitura.js
+++ js/leitura.js
@@ -1,4 +1,9 @@
 import dados from './cliente.json' assert { type: "json" };
 
 console.log(dados);
-console.log(typeof dados);
+console.log(`É do tipo: ${typeof dados}`);
+
+const clientEmString = JSON.stringify(dados);
+
+console.log(clientEmString);
+console.log(`É do tipo: ${typeof clientEmString}`);

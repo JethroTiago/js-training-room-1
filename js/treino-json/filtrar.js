3e2057bc4d941155add08d8ad8d81b5908c75d08
Jethro <jethrotiago@hotmail.com>
2023-02-22 12:18:08 -0300

    Aprimorando função para filtrar apartamentos

--- js/treino-json/filtrar.js
+++ js/treino-json/filtrar.js
@@ -3,6 +3,9 @@ import clientes from "./clientes.json" assert { type: "json" };
 function filtrarApartamentosSemComplemento(clientes) {
     return clientes.filter((cliente) => {
         return (cliente.endereco.apartamento && 
-            cliente.endereco.hasOwnProperty("complemento"))
+            !cliente.endereco.hasOwnProperty("complemento"))
     });
 }
+
+const filtrados = filtrarApartamentosSemComplemento(clientes);
+console.log(filtrados);
